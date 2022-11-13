cea13b698475482fee84933f89e26b3cad077986
Joao Castro <joaocastro186@gmail.com>
2022-11-13 20:07:02 -0300

    mudanca back adicionar atributo no mapeamento

--- Back-Quarkus/src/main/java/dev/socialnetwork/socialnetwork/rest/dto/FollowersResponse.java
+++ Back-Quarkus/src/main/java/dev/socialnetwork/socialnetwork/rest/dto/FollowersResponse.java
@@ -8,15 +8,18 @@ public class FollowersResponse {
     private Long id;
     private String name;
 
+    private  Long followerId;
+
     public FollowersResponse() {
     }
 
     public FollowersResponse(Followers followers){
-        this(followers.getId(), followers.getFollower().getName());
+        this(followers.getId(), followers.getFollower().getName(),followers.getFollower().getId());
     }
 
-    public FollowersResponse(Long id, String name) {
+    public FollowersResponse(Long id, String name,Long followerId) {
         this.id = id;
         this.name = name;
+        this.followerId = followerId;
     }
 }
